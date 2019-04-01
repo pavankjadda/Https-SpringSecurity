7b15e66a96a9e4750c3099293c3e0ea5aa89ec7b
Pavan Jadda <jpavanaryan@gmail.com>
2019-03-31 22:55:23 -0400

    Spring Boot version updated to 2.1.3.RELEASE

--- src/main/java/com/springtesting/SpringTestingApplication.java
+++ src/main/java/com/springtesting/SpringTestingApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringtestingApplication
+public class SpringTestingApplication
 {
     public static void main(String[] args)
     {
-        SpringApplication.run(SpringtestingApplication.class, args);
+        SpringApplication.run(SpringTestingApplication.class, args);
     }
 }
 
