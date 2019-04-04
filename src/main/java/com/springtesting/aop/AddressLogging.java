4fbdf0a7c1f094145d51c6ebd20d7dcda346c66f
Pavan Jadda <jpavanaryan@gmail.com>
2019-04-03 22:18:21 -0400

    Updated Sql Commands

--- src/main/java/com/springtesting/aop/AddressLogging.java
+++ src/main/java/com/springtesting/aop/AddressLogging.java
@@ -1,6 +1,5 @@
 package com.springtesting.aop;
 
-import com.springtesting.model.Address;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
@@ -9,7 +8,6 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.domain.Page;
 
 
 
@@ -17,25 +15,27 @@ import org.springframework.data.domain.Page;
 @Configuration
 public class AddressLogging
 {
-    private Logger log=LoggerFactory.getLogger(AddressLogging.class);
+    private Logger logger = LoggerFactory.getLogger(AddressLogging.class);
 
-    //@Pointcut("execution(* com.springtesting.repo.AddressRepository.*(..))")
     @Pointcut("execution(* com.springtesting.repo.AddressRepository.findAll())")
-    public void getAddresses() {}
+    public void getAddresses()
+    {
+        //Pointcut expression
+    }
 
     @After("getAddresses()")
     public void afterAdvice()
     {
-        log.info("Log Message: Inside afterAdvice() advice");
+        logger.info("Log Message: Inside afterAdvice() advice");
     }
 
     @AfterReturning(pointcut = "execution(* com.springtesting.repo.AddressRepository.*(..))",returning = "addressList")
     public void afterReturningAdvice(JoinPoint jp, Object addressList)
     {
-        System.out.println("Method Signature: "  + jp.getSignature());
-        Page<Address> pagedAddressList= (Page<Address>) addressList;
+        logger.info("Method Signature: {}", jp.getSignature());
+        /*Page<Address> pagedAddressList= (Page<Address>) addressList;
         for (Address address:pagedAddressList)
-            System.out.println(address.toString());
+            System.out.println(address.toString());*/
     }
 
 }
