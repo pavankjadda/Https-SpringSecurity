bf516c8ac40172dc74f1de0c8acf49aaeafaf294
pjadda <jpavanaryan@gmail.com>
2019-05-18 00:44:05 -0400

    Angular UI Added

--- src/main/resources/sql/User Data SQL commands.sql
+++ src/main/resources/sql/User Data SQL commands.sql
@@ -1,9 +1,10 @@
 
 /* Insert Data into User Table  */
-INSERT INTO `httpsspringsecurity`.`user`(`id`, `active`, `username`, `password`)
-VALUES (1001, true, 'admin', '$2a$12$lkl.PF9eGBW/iofHhophlOWD3KEmSKqMXYrufsU9g5TMnio7N8JOK');
-INSERT INTO `httpsspringsecurity`.`user`(`id`, `active`, `username`, `password`)
-VALUES (1002, true, 'user', '$2a$12$tBTv6hgDYME.QePjkXEdxOnYhi95zPtamm6NskNuSA5XeTtJKGhzS');
+INSERT INTO `httpsspringsecurity`.`user`(`id`,`account_non_expired`,`account_non_locked`,`active`,`credentials_non_expired`,`password`,`username`)
+VALUES (1001, true, true, true, true, '$2a$12$BJ35i2beLtpO4r.b04FZeu8Yn/fxJF.2xg15e29j.FzCpd3QV8eAS' , 'admin');
+
+INSERT INTO `httpsspringsecurity`.`user`(`id`,`account_non_expired`,`account_non_locked`,`active`,`credentials_non_expired`,`password`,`username`)
+VALUES (1002, true, true, true, true, '$2a$12$tBTv6hgDYME.QePjkXEdxOnYhi95zPtamm6NskNuSA5XeTtJKGhzS' , 'user');
 
 
 insert into httpsspringsecurity.role values(1,'ROLE_USER');
@@ -22,6 +23,7 @@ insert into httpsspringsecurity.privilege values(3,'DELETE_PRIVILEGE');
 INSERT INTO `httpsspringsecurity`.`user_role`(`id`,`user_id`,`role_id`) VALUES (1,2,1);
 INSERT INTO `httpsspringsecurity`.`user_role`(`id`,`user_id`,`role_id`) VALUES (2,1,2);
 INSERT INTO `httpsspringsecurity`.`user_role`(`id`,`user_id`,`role_id`) VALUES (3,1,1);
+INSERT INTO `httpsspringsecurity`.`user_role`(`id`,`user_id`,`role_id`) VALUES (4,1,3);
 
 /* Insert Data into RolePrivilege Table  */
 insert into httpsspringsecurity.role_privilege values(2,1);
