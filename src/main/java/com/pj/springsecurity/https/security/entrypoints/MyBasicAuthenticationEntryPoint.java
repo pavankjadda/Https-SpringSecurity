01e2ed555d8aa4d557236ed6ccd3244f7c8a63fa
Pavan Jadda <jpavanaryan@gmail.com>
2019-11-19 16:27:46 -0500

    Upgrade to Spring Boot 2.2.1

--- src/main/java/com/pj/springsecurity/https/security/entrypoints/MyBasicAuthenticationEntryPoint.java
+++ src/main/java/com/pj/springsecurity/https/security/entrypoints/MyBasicAuthenticationEntryPoint.java
@@ -4,7 +4,6 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -15,8 +14,7 @@ public class MyBasicAuthenticationEntryPoint extends BasicAuthenticationEntryPoi
 {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authEx)
-            throws IOException, ServletException
-    {
+            throws IOException {
         response.addHeader("WWW-Authenticate", "Basic realm='' + getRealmName() + ''");
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         PrintWriter writer = response.getWriter();
@@ -24,8 +22,7 @@ public class MyBasicAuthenticationEntryPoint extends BasicAuthenticationEntryPoi
     }
 
     @Override
-    public void afterPropertiesSet() throws Exception
-    {
+    public void afterPropertiesSet() {
         setRealmName("SpringSecuritySpringData");
         super.afterPropertiesSet();
     }
